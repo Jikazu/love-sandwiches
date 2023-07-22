9fffe39810980e54cd550ad45c2f11fcbfe4f57e
Gabriella Köhler <gabriella.kohler91@gmail.com>
2023-07-22 13:04:21 +0000

    add requirements for deployment

--- run.py
+++ run.py
@@ -4,7 +4,7 @@
 
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -26,7 +26,7 @@ def get_sales_data():
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here:\n")
 
         sales_data = data_str.split(",")
         
@@ -78,8 +78,8 @@ def calculate_surplus_data(sales_row):
     print("Calculating surplus data...\n")
     stock= SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(f"Stock row: {stock_row}")
-    print(f"Sales row: {sales_row}")
+    #print(f"Stock row: {stock_row}")
+    #print(f"Sales row: {sales_row}")
 
     surplus_data = []
     for stock, sales in zip(stock_row, sales_row):
@@ -104,6 +104,21 @@ def get_last_5_entries_sales():
 
     return columns
 
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each market
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
 def main():
     """
     Run all program functions
@@ -113,11 +128,12 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 print("Welcome to Love Sandwiches data Automation!")
-#main()
-
-sales_columns = get_last_5_entries_sales()
+main()
 
 
 """
